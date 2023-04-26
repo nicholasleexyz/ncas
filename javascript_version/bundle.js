121d60442a5d024cd376be0ea0aba7d112b7a9a6
Nicholas Lee <nicholasleexyz@gmail.com>
2023-04-26 16:25:24 -0600

    size wip

--- javascript_version/bundle.js
+++ javascript_version/bundle.js
@@ -970,17 +970,42 @@ let seed = "asdf";
 
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
-const width = canvas.width = 128;
-const height = canvas.height = 128;
+var width = canvas.width = 128;
+var height = canvas.height = 128;
 const cellSize = 1;
-const cols = Math.floor(width / cellSize);
-const rows = Math.floor(height / cellSize);
+var grid = [];
+
+// const width = Math.floor(width / cellSize);
+// const height = Math.floor(height / cellSize);
 
 // let density = 0.6;
 // const density_input = document.querySelector('#density input');
 // const density_label = document.querySelector('#density span');
 
 const seed_input = document.querySelector('#seed input');
+
+var ele = document.getElementsByName('size');
+for (let i = 0; i < ele.length; i++) {
+    const element = ele[i];
+
+    let change = () => {
+        let s = 0;
+
+        if(element.id === "small")
+            s= 128;
+        else if(element.id === "medium")
+            s= 256;
+        else if(element.id === "large")
+            s= 512;
+
+        width = canvas.width = s;
+        height = canvas.height = s;
+
+        generateGrid();
+    };
+
+    element.onclick = change();
+}
 
 // density_input.value = density;
 // let decimalToPercent = (decimal) =>
@@ -998,10 +1023,9 @@ seed_input.addEventListener('input', () => {
     generateGrid();
 });
 
-let grid = [];
 
 function generateGrid() {
-    grid = new Array(cols).fill().map(() => new Array(rows));
+    grid = new Array(width).fill().map(() => new Array(height));
 
     grid = generateWanderingDrunkardNoise(seed, 1);
 
@@ -1016,12 +1040,12 @@ function generateGrid() {
 }
 
 function generateCellularAutomata(noise) {
-    let ca = new Array(cols).fill().map(() => new Array(rows));
+    let ca = new Array(width).fill().map(() => new Array(height));
     let current_noise = noise.map(a => a);
 
-    for (let y = 0; y < rows; y++) {
-        for (let x = 0; x < cols; x++) {
-            if (y > 0 && y < rows - 1 && x > 0 && x < cols - 1) {
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            if (y > 0 && y < height - 1 && x > 0 && x < width - 1) {
                 let neighbors = [
                     current_noise[y + 1][x - 1],
                     current_noise[y + 1][x],
@@ -1071,15 +1095,15 @@ function generateWanderingDrunkardNoise(seed, numOfDrunkards) {
 
     let noise = [];
 
-    for (let y = 0; y < rows; y++) {
+    for (let y = 0; y < height; y++) {
         noise[y] = [];
-        for (let x = 0; x < cols; x++) {
+        for (let x = 0; x < width; x++) {
             noise[y][x] = grid[y][x];
         }
     }
 
-    let xCoord = Math.round(rng() * cols) % cols;
-    let yCoord = Math.round(rng() * rows) % rows;
+    let xCoord = Math.round(rng() * width) % width;
+    let yCoord = Math.round(rng() * height) % height;
 
     for (let i = 0; i < numOfDrunkards; i++) {
         let steps = 1024 * 4;
@@ -1090,9 +1114,9 @@ function generateWanderingDrunkardNoise(seed, numOfDrunkards) {
 
             for (let z = 0; z < 1; z++) {
                 if (dir == 0)
-                    noise[yCoord > rows - 2 ? yCoord : ++yCoord][xCoord] = 1;
+                    noise[yCoord > height - 2 ? yCoord : ++yCoord][xCoord] = 1;
                 else if (dir == 1)
-                    noise[yCoord][xCoord > rows - 2 ? xCoord : ++xCoord] = 1;
+                    noise[yCoord][xCoord > height - 2 ? xCoord : ++xCoord] = 1;
                 else if (dir == 2)
                     noise[yCoord == 0 ? yCoord : --yCoord][xCoord] = 1;
                 else if (dir == 3)
@@ -1143,8 +1167,8 @@ generateGrid();
 // }
 
 function drawGrid() {
-    for (let y = 0; y < rows; y++) {
-        for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
             // context.fillStyle = grid[y][x] ? "#222E44" : "#3497A1";
             // context.fillStyle = grid[y][x] ? "#5B76B9" : "#63C7DD" ;
             // context.fillStyle = grid[y][x] ? "#3A2B2A" : "#F5F6F7" ;
