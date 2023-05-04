c3ad9befec7c0d4022a3c005125050d7047e44a5
Nicholas Lee <nicholasleexyz@gmail.com>
2023-05-04 01:37:21 -0600

    data download tweaks

--- javascript_version/src/main.js
+++ javascript_version/src/main.js
@@ -227,16 +227,12 @@ download_button.addEventListener('click', (e) => {
     const link = document.createElement("a");
     let flattened = [].concat(...grid);
     let total = [];
-    for (let i = 0; i < flattened.length; i++) {
-        let val = flattened[i];
-        total.push(val);
-        if((i + 1) % width == 0)
-            total.push("\n");
-    }
+    for (let i = 0; i < flattened.length; i++)
+        total.push((i + 1) % width == 0 ? flattened[i] + "\n" : flattened[i]);
 
     let file = new Blob(total, {type: 'text/plaintext'});
     link.href = URL.createObjectURL(file);
-    link.download = "asdf.txt"
+    link.download = `${seed_input.value}.txt`;
     link.click();
     URL.revokeObjectURL(link.href);
 
